ab1c2cc3ed9d112918b408d10a144d92d9782e91
Dhamith Hewamullage <dhamith.h@gmail.com>
2023-05-31 15:58:03 +0530

    UI self key and IP display

--- cmd/gui/frontend/src/App.svelte
+++ cmd/gui/frontend/src/App.svelte
@@ -1,16 +1,34 @@
 <script>
   import DevicesTab from "./components/DevicesTab.svelte";
   import {GetKey} from '../wailsjs/go/main/App.js'
+  import {GetIp} from '../wailsjs/go/main/App.js'
 
+  let ip = '';
   let key = '';
-  GetKey().then(result => key += result)
+  GetKey().then(result => key = result)
+  GetIp().then(result => ip = result);
 </script>
 
 <main>
-    {key}
+    <div id="meta">
+        <div>
+            <h3>IP: {ip}</h3>  
+        </div>
+        <div>
+            <h3>Key: {key}</h3>
+        </div>
+    </div>
     <DevicesTab /> 
 </main>
 
 <style>
+    #meta {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        gap: 50px;
+        height: 30px;
+    }
 
 </style>
