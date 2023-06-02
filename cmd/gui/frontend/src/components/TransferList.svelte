1b1b8ed7ff5e22324361ebdb79549699f59b3632
Dhamith Hewamullage <dhamith.h@gmail.com>
2023-06-02 21:13:56 +0530

    Frontend handling file transfers queues and cancellations

--- cmd/gui/frontend/src/components/TransferList.svelte
+++ cmd/gui/frontend/src/components/TransferList.svelte
@@ -1,68 +1,69 @@
 <script>
+    import {GetTransfers} from '../../wailsjs/go/main/App.js'
+    import {CancelTransfer} from '../../wailsjs/go/main/App.js'
     import TransferItem from './TransferItem.svelte';
 
-    let transfers = [
-        {
-            component: TransferItem,
-            filename: 'some_file.txt',
-            ip: '192.168.111.111',
-            size: '7.5Mb',
-            completedSize: '6.6Mb',
-            eta: '1m 25s',
-            speed: '12.4 KB/s',
-            done: 40,
-            isDownload: false,
-            isCanceled: false
-        },
-        {
-            component: TransferItem,
-            filename: 'some_file2.txt',
-            ip: '192.168.111.111',
-            size: '7.5Mb',
-            completedSize: '6.6Mb',
-            eta: '1m 25s',
-            speed: '12.4 KB/s',
-            done: 80,
-            isDownload: true,
-            isCanceled: false
-        },
-        {
-            component: TransferItem,
-            filename: 'some_file2.txt',
-            ip: '192.168.111.111',
-            size: '7.5Mb',
-            completedSize: '6.6Mb',
-            eta: '1m 25s',
-            speed: '12.4 KB/s',
-            done: 100,
-            isDownload: true,
-            isCanceled: false
-        },
-        {
-            component: TransferItem,
-            filename: 'some_file2.txt',
-            ip: '192.168.111.111',
-            size: '7.5Mb',
-            completedSize: '6.6Mb',
-            eta: '1m 25s',
-            speed: '12.4 KB/s',
-            done: 80,
-            isDownload: true,
-            isCanceled: false
-        },
-        {
-            component: TransferItem,
-            filename: 'some_file2.txt',
-            ip: '192.168.111.111',
-            size: '7.5Mb',
-            completedSize: '6.6Mb',
-            eta: '1m 25s',
-            speed: '12.4 KB/s',
-            done: 100,
-            isDownload: true,
-            isCanceled: false
-        }
-    ];
+    export let showTransfers;
+    let transfers = [];
+
+    const calculateSpeed = (timeInSecs, bytes) => {
+        return bytes / timeInSecs;
+    };
+
+    const convertToHumanReadableTime = (seconds) => {
+        let hours   = Math.floor(seconds / 3600)                 
+        let minutes = Math.floor((seconds - (hours * 3600)) / 60)
+        seconds = seconds - (hours * 3600) - (minutes * 60)  
+        if ( !!hours ) {                                         
+            if ( !!minutes ) {                                     
+                return `${hours}h ${minutes}m ${seconds.toFixed(2)}s`           
+            } else {                                               
+                return `${hours}h ${seconds.toFixed(2)}s`                       
+            }                                                      
+        }                                                        
+        if ( !!minutes ) {                                       
+            return `${minutes}m ${seconds.toFixed(2)}s`                       
+        }                                                        
+        return `${seconds.toFixed(2)}s`
+    };
+
+    const cancelFunc = (ip, filename, isDownload) => {
+        console.log(ip, filename, isDownload)
+        CancelTransfer(ip, filename, isDownload).catch(e => {console.log(e)});
+    };
+
+    $: if (showTransfers) {
+        setInterval(() => {
+            GetTransfers().then((res) => {
+                let ts = [];
+                res.forEach(t => {
+                    let endTime = (t.EndTime === '') ? Math.floor(Date.now() / 1000) : t.EndTime;
+                    let timeDiff = endTime - t.StartTime;
+                    let speed = calculateSpeed(timeDiff, t.CompletedBytes);
+                    let estimated = (t.SizeBytes - t.CompletedBytes) / speed;
+                    ts.push({
+                        component: TransferItem,
+                        filename: t.Name,
+                        path: t.Path,
+                        ip: t.Ip,
+                        size: `${(t.SizeBytes / 1024 / 1024).toFixed(2)} MB`,
+                        completedSize: `${(t.CompletedBytes / 1024 / 1024).toFixed(2)} MB`,
+                        eta: convertToHumanReadableTime(estimated.toFixed(2)),
+                        timeSpent: convertToHumanReadableTime(timeDiff.toFixed(2)),
+                        speed: (speed > 1000000) ? `${(speed/1024/1024).toFixed(2)} MB/s` : `${(speed/1024).toFixed(2)} KB/s`,
+                        done: (t.CompletedBytes / t.SizeBytes) * 100,
+                        isDownload: t.IsDownload,
+                        isCanceled: t.Status === 'cancelled' || t.Status === 'error',
+                        status: t.Status,
+                        cancelFunc: cancelFunc
+                    })
+                });
+                transfers = ts;
+            }).catch(e => {
+                console.log(e);
+            });
+        }, 1000);
+    }
 </script>
 
 <div id="main">
